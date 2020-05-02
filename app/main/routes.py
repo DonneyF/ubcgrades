5692fda42584c4388bd87f31e8aca4b5246fe0fc
Donney Fan <donney.fan@hotmail.com>
2020-05-02 00:11:09 -0700

    Add Argon template. Place placeholder content for View Grades and Statistics by Course.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,12 @@
 from app.main import bp
+from flask import render_template
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET'])
 def index():
-    return "Hello World!"
+    return render_template('index.html', header_card_text="View Grades")
+
+
+@bp.route('/statistics-by-course', methods=['GET'])
+def statistics_by_course():
+    return render_template('statistics_by_course.html', header_card_text="Statistics by Course")
--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSONIFY_PRETTYPRINT_REGULAR = True
+    TEMPLATES_AUTO_RELOAD = True
